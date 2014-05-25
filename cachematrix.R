c8779b301b7e32eaba20c095125acc06baf5c892
hali123 <hafees@gmail.com>
2014-05-25 17:08:06 -0400

    committing comments to assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,26 @@
-## The following function creates a special matrix object called makecachematrix. 
-## makecachematrix is a list of functions that sets and gets the value of a given matrix
+## The following function creates a special matrix object called makeCacheMatrix. 
+## makeCacheMatrix is a list of functions that sets and gets the value of a given matrix
 ## as well as sets and gets the inverse of that matrix.
 
-makecachematrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {
         
         i <- NULL        
+        
+        ## The following function sets the global value of x and initializes i.
+        
         set <- function(y){
                 x <<- y
                 i <<- NULL
         }
+        
+        ## The following piece of code returns the value of x, sets and returns the global value of i.
+        
         get <- function() x
         setinv <- function(inv) i <<- inv
         getinv <- function() i
+        
+        ## The following piece of code creates a list including set, get, setinv and getinv as defined above
+        
         list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
@@ -21,13 +30,19 @@ makecachematrix <- function(x = matrix()) {
 ## and the matrix has not changed then cachesolve should retrieve the inverse from the cache
 
 cachesolve <- function(x, ...) {
+
+        ## The following piece of code returns the inverse of matrix x from the cache.
         
         i <- x$getinv()
+        
+        ## The following piece of code returns the message "inverse from cache" if the value exists in cache.        
         
         if(!is.null(i)){
                 message("inverse from cache")
                 return(i)
         }
+        
+        ## The following piece of code calculates the inverse for the first time and also populates the cache
         
         z <- x$get()
         i <- solve(z)
